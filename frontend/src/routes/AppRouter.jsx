ab8c1ac3782f8ff3c65aa6669b43920c8c52c3b8
m1yaryu <ryuschoolacct@gmail.com>
2025-12-19 03:09:32 +0800

    feat(routing): initialize routing

---
+++ frontend/src/routes/AppRouter.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
+const AppRouter = () => {
+    <Routes>
+        <Route path="/" element={}/>
+    </Routes>
+}
+
+export default AppRouter;
