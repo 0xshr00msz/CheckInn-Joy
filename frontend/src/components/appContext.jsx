1337908db1e5cc647896d35dc3c491a7b2eaf2cc
m1yaryu <ryuschoolacct@gmail.com>
2025-12-23 03:12:01 +0800

    feat(global-state-management): init gsm

---
+++ frontend/src/components/appContext.jsx
@@ -0,0 +1,9 @@
+import { createContext, useContext, useState, useEffect, Children } from "react";
+
+const context = createContext([])
+
+const contextProvider = ({children}) => {
+
+}
+
+export {context, contextProvider}
